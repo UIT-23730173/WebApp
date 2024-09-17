c5d6132060c9471422abe0577549ed5a08a70ed4
Van Thanh Huy <23730173@ms.uit.edu.vn>
2024-09-17 19:15:48 +0700

    Handle data giao hang

--- js/custom-jquery.js
+++ js/custom-jquery.js
@@ -1,7 +1,7 @@
 /* Huy Van is the original author of this 
 This is to handle the function effects in Mini App
 */function deleteData() {
-  if (confirm("Xóa toàn bộ dữ liệu về đơn hàng hiện tại và số đơn hàng?") == false) {
+  if (confirm("Xóa toàn bộ dữ liệu về đơn hàng hiện tại và số đơn hàng?") == true {
     localStorage.clear();
     location.reload();
   }
