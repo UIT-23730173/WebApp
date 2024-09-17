53a3c6410aa8905f33992bc36ddc60a78fcd5aa3
Van Thanh Huy <23730173@ms.uit.edu.vn>
2024-09-17 18:55:20 +0700

    Function to handle Xoa data

--- js/custom-jquery.js
+++ js/custom-jquery.js
@@ -1,3 +1,11 @@
 /* Huy Van is the original author of this 
 This is to handle the function effects in Mini App
 */
+
+//Handle delete data
+function deleteData() {
+    if (confirm("Xóa toàn bộ dữ liệu về đơn hàng hiện tại và số đơn hàng?") === true) {
+      localStorage.clear();
+      location.reload();
+    }
+  }
