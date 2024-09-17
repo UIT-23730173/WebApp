52f40f9877a93c0b4d658b9f10b07032e82d3ce4
Ngan Nguyen | Isuzu Vietnam <ngannhk@isuzu-vietnam.com>
2024-09-17 17:56:37 +0700

    Xu ly data

--- js/custom-jquery.js
+++ js/custom-jquery.js
@@ -1,3 +1,8 @@
 /* Huy Van is the original author of this 
 This is to handle the function effects in Mini App
-*/
+*/function deleteData() {
+  if (confirm("Xóa toàn bộ dữ liệu về đơn hàng hiện tại và số đơn hàng?") == false) {
+    localStorage.clear();
+    location.reload();
+  }
+}
