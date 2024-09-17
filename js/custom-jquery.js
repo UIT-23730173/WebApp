bc5d730a4323ec093242b731fefb2549c3bfa851
Van Thanh Huy <23730173@ms.uit.edu.vn>
2024-09-17 19:17:12 +0700

    Handle giao hang visible

--- js/custom-jquery.js
+++ js/custom-jquery.js
@@ -4,7 +4,7 @@ This is to handle the function effects in Mini App
 
 //Handle delete data
 function deleteData() {
-    if (confirm("Xóa toàn bộ dữ liệu về đơn hàng hiện tại và số đơn hàng?") === true) {
+    if (confirm("Xóa toàn bộ dữ liệu về đơn hàng hiện tại và số đơn hàng?") == true) {
       localStorage.clear();
       location.reload();
     }
